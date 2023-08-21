54891b7aa68811924045b72262b9c8391d39c23c
Zhanna Kulagina <kuljeanne@gmail.com>
2023-08-21 18:03:58 +0300

    add base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProtectedRouteComponent } from './protected-route/protected-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: ProtectedRouteComponent,
+    children: [
+      { path: '', component: PostsComponent },
+      { path: ':postId', component: PostDetailComponent },
+    ],
+  },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'sign-in', component: AuthFormComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './posts/posts.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProtectedRouteComponent } from './protected-route/protected-route.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostDetailComponent,
+    AuthFormComponent,
+    PageNotFoundComponent,
+    ProtectedRouteComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
