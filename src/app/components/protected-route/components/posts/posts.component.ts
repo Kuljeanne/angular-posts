b695f22e3396010d343c232d90327c1d23968d0e
Zhanna Kulagina <kuljeanne@gmail.com>
2023-08-23 23:35:37 +0300

    feat: pagination

--- src/app/components/protected-route/components/posts/posts.component.ts
+++ src/app/components/protected-route/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { map } from 'rxjs';
 import { PostsService } from 'src/app/services/posts.service';
 import { UsersService } from 'src/app/services/users.service';
@@ -13,12 +14,17 @@ export class PostsComponent implements OnInit {
   public posts: IPost[] = [];
   public users: IUser[] = [];
 
+  public pageSize = 10;
+  public pageIndex = 0;
+  public pageSizeOptions = [5, 10, 25];
+  public visiblePosts: IPost[] = this.posts.slice(0, this.pageSize);
+
   constructor(
     private postService: PostsService,
     private userService: UsersService
   ) {}
 
-  getUserName = this.userService.getUserName
+  getUserName = this.userService.getUserName;
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((users) => {
@@ -26,6 +32,24 @@ export class PostsComponent implements OnInit {
     });
     this.postService.getPosts().subscribe((posts) => {
       this.posts = posts;
+      this.visiblePosts = this.posts.slice(0, this.pageSize);
     });
+  }
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent!: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+
+    const start = this.pageIndex * this.pageSize;
+    const end = start + this.pageSize;
+    this.visiblePosts = this.posts.slice(start, end);
   }
 }
